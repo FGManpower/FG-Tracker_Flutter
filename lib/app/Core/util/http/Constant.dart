ae30878a51973a4b4022f1470943622e1a2d2cf0
fgmanpower <sabdulsamad272@gmail.com>
2025-09-29 17:12:40 +0530

    fixed some bugs

--- lib/app/Core/util/http/Constant.dart
+++ lib/app/Core/util/http/Constant.dart
@@ -3,16 +3,16 @@ import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 String ProductionUrl = "http://fgtracker.in:3000/";
 String development = "http://192.168.152.223:4000/";
-// String socketUrl = "http://fgtracker.in:3000"; //pro
-String socketUrl = "http://192.168.152.223:4000"; //dev
+String socketUrl = "http://fgtracker.in:3000"; //pro
+// String socketUrl = "http://192.168.152.223:4000"; //dev
 
 final String gMapApiKey = "AIzaSyDu1PPgXuqRdLfEfA9Gf-6A8QydUlyMq-0";
 final String agoraAppId = "46901509bdc9411a85d1287c9957a42d";
 
 class Constant {
-  static String Baseurl = "${development}api";
+  static String Baseurl = "${ProductionUrl}api";
   static String incomingDeepLinkUrl = "https://fgtracker.in";
-  static String ImagebaseUrl = development;
+  static String ImagebaseUrl = ProductionUrl;
   static BaseOptions networkOptions = BaseOptions(
     baseUrl: Baseurl,
   );
